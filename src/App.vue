fb6383c35bb7f0c121f5ecb5ca1f02961709cfcd
Baljit Singh <baljit@bitcot.com>
2020-10-13 17:37:13 +0530

    added calendar

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,65 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <FullCalendar @dateClick="handleDateClick" :options="calendarOptions" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FullCalendar from "@fullcalendar/vue";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    FullCalendar, // make the <FullCalendar> tag available
+  },
+  data() {
+    return {
+      calendarOptions: {
+        plugins: [
+          dayGridPlugin,
+          interactionPlugin, // needed for dateClick
+        ],
+        headerToolbar: {
+          left: "prev,next today",
+          center: "title",
+          right: "dayGridMonth,dayGridWeek,dayGridDay",
+        },
+        initialView: "dayGridMonth",
+        editable: true,
+        selectable: true,
+        selectMirror: true,
+        dayMaxEvents: true,
+        weekends: true,
+        select: this.handleDateSelect,
+        eventClick: this.handleEventClick,
+        eventsSet: this.handleEvents,
+        /* you can update a remote database when these fire:
+        eventAdd:
+        eventChange:
+        eventRemove:
+        */
+        events: [
+          { title: "event 1", date: "2020-10-11" },
+          { title: "event 2", date: "2020-10-12" },
+          { title: "event 3", date: "2020-10-4" },
+          { title: "event 4", date: "2020-10-14" },
+          { title: "event 5", date: "2020-10-16" },
+          { title: "event 6", date: "2020-10-30" },
+        ],
+      },
+    };
+  },
+  methods: {
+    handleDateClick: function (arg) {
+      alert("date click! " + arg.dateStr);
+    },
+    handleWeekendsToggle() {
+      this.calendarOptions.weekends = !this.calendarOptions.weekends; // update a property
+    },
+  },
+};
 </script>
 
 <style>
